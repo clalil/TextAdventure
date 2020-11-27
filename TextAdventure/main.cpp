ffb0b4584a09685644b871a21767cb9a81be09a7
clalil <cliljander@gmail.com>
2020-11-27 18:49:46 +0100

    Update game structure, add game class & remodel code.

--- TextAdventure/main.cpp
+++ TextAdventure/main.cpp
@@ -5,10 +5,10 @@
 //  Created by Clarissa Liljander on 2020-11-18.
 //  Copyright © 2020 Clarissa Liljander. All rights reserved.
 //
-#include "game_data.hpp"
+#include "game.hpp"
 #include <iostream>
+Game game;
 
 int main() {
-    GameData data;
-    data.DebugLocations();
+    game.run();
 }
