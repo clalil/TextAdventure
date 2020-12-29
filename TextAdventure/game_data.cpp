59bcbf5c89e07fde91b3ba430deaa63e4ecfae24
clalil <cliljander@gmail.com>
2020-12-29 16:41:35 +0100

    Update txt file to increased readibility changing % to |

--- TextAdventure/game_data.cpp
+++ TextAdventure/game_data.cpp
@@ -206,9 +206,9 @@ const int GameData::LoadLocationData(const std::string path) {
             current_location->location_items.push_back(line.substr(1));
 
         } else if (loc_choice_id != std::string::npos) {
-            std::shared_ptr<LocationChoice> current_location_choice = std::make_shared<LocationChoice>(FindString(line, "", 1, "%", 1), FindString(line, ":", 2, "", 0));
+            std::shared_ptr<LocationChoice> current_location_choice = std::make_shared<LocationChoice>(FindString(line, "", 1, "|", 1), FindString(line, ":", 2, "", 1));
 
-            current_location_choice->required_item_id = FindString(line, "%", 1, "+", 1);
+            current_location_choice->required_item_id = FindString(line, "|", 1, "+", 1);
             current_location_choice->hidden_item_id = FindString(line, "+", 1, ":", 1);
 
             current_location->choices.push_back(current_location_choice);
